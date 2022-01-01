83791106df23dfa389cd8a73c22d7edb979f1e9a
Neo64YT <naufalramli06@gmail.com>
2022-01-01 21:57:05 +0800

    Added config for wget

--- autostart/.config/autostart-cmds/autostart.sh
+++ autostart/.config/autostart-cmds/autostart.sh
@@ -6,21 +6,20 @@
 # To prevent autostarting a program using XDG autostart, create an empty desktop file for that program in $XDG_CONFIG_HOME/autostart/
 dex -a &
 
-# Background daemons first
+# Daemons 
 pipewire-runner &
 picom --experimental-backend &
 /usr/bin/dunst &
-spacefm -d &
-devmon &
+thunar --daemon &
 $LOCK &
 sxhkd -c $KEYBIND &
 xrdb -merge $XDG_CONFIG_HOME/Xresources &
+steam -silent &
 
 # Systray
-# Bluetooth is disabled due to vulnerabilities
-mictray &
 indicator-kdeconnect &
 pnmixer &
+
 
 
 # vim: ft=sh
